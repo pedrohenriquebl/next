1e0b31ee9cc336ca76d18ba72804ffadf0332519
Pedro Henrique <pedruhbl@yahoo.com.br>
2024-09-03 15:16:04 -0300

    :construction: feat: remove boilerplate files and create Header component

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,19 @@
+import styles from './header.module.css';
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <Image
+                src="/author.svg"
+                alt="Author Logo"
+                className={styles.logo}
+                width={64}
+                height={64}
+                priority
+            />
+            <h1 className={styles.title}>DESIGN BETTER. FASTER. TOGETHER.</h1>
+            <h2 className={styles.subtitle}>The digital product design platform powering the world’s best user experiences</h2>
+        </header>
+    );
+}
