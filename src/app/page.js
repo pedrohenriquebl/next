65f2d7a9aeddd42b11a8a99867b31b3a150b5de3
Pedro Henrique <pedruhbl@yahoo.com.br>
2024-09-05 15:54:49 -0300

    :construction: feat: Add metadata

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Head from 'next/head';
 import Header from "./components/Header/Header";
 import Post from "./components/Post/Post"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header />
-      <Post />
-    </main>
+      <>
+          <Head>
+              <title>Sudeste Online</title>
+              <meta name="description" content="Sudeste Online Figma Layout"/>
+              <meta property="og:title" content="Sudeste Online Figma Layout"/>
+              <meta property="og:description" content="Sudeste Online Figma Layout"/>
+              <meta property="og:image" content="https://lh3.googleusercontent.com/p/AF1QipNpVP9ge5yKt6h7sXBQXiKFad5R7NqaEWvjF4Kr=s680-w680-h510"/>
+              <meta property="og:url" content="https://sudesteonline.com.br/"/>
+              <meta property="og:type" content="website"/>
+          </Head>
+          <main className={styles.main}>
+              <Header/>
+              <Post/>
+          </main>
+      </>
   );
 }
